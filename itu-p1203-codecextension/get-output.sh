621e97218e80a7c60c9308ffe93ae29a197e4850
JesusAguilar95 <56531350+JesusAguilar95@users.noreply.github.com>
2021-10-28 11:21:34 +0200

    Update get-output.sh

--- itu-p1203-codecextension/get-output.sh
+++ itu-p1203-codecextension/get-output.sh
@@ -19,27 +19,4 @@ cd itu-p1203-codecextension
 
 ./calculate.py TestbedEvaluation-0.json >> output0.txt
 ./calculate.py TestbedEvaluation-1.json >> output1.txt
-./calculate.py TestbedEvaluation-2.json >> output2.txt
-./calculate.py TestbedEvaluation-3.json >> output3.txt
-./calculate.py TestbedEvaluation-4.json >> output4.txt
-./calculate.py TestbedEvaluation-5.json >> output5.txt
-./calculate.py TestbedEvaluation-6.json >> output6.txt
-./calculate.py TestbedEvaluation-7.json >> output7.txt
-./calculate.py TestbedEvaluation-8.json >> output8.txt
-./calculate.py TestbedEvaluation-9.json >> output9.txt
-./calculate.py TestbedEvaluation-10.json >> output10.txt
-./calculate.py TestbedEvaluation-11.json >> output11.txt
-./calculate.py TestbedEvaluation-12.json >> output12.txt
-./calculate.py TestbedEvaluation-13.json >> output13.txt
-./calculate.py TestbedEvaluation-14.json >> output14.txt
-./calculate.py TestbedEvaluation-15.json >> output15.txt
-./calculate.py TestbedEvaluation-16.json >> output16.txt
-./calculate.py TestbedEvaluation-17.json >> output17.txt
-./calculate.py TestbedEvaluation-18.json >> output18.txt
-./calculate.py TestbedEvaluation-19.json >> output19.txt
-./calculate.py TestbedEvaluation-20.json >> output20.txt
-./calculate.py TestbedEvaluation-21.json >> output21.txt
-./calculate.py TestbedEvaluation-22.json >> output22.txt
-./calculate.py TestbedEvaluation-23.json >> output23.txt
-./calculate.py TestbedEvaluation-24.json >> output24.txt
 
